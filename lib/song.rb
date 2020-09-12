27675bc1f4438b7713f8e8463a92bb38265bcfdb
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-12 04:08:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -66,6 +66,22 @@ end
 
 
 def self.create_from_filename(song)
+  artist_name = song.split(" - ")[0] 
+ 
+  song_name = song.split(" - ")[1]
+ 
+  song_name = song_name.split(".mp3")[0]
+ 
+  new_song = Song.new
+
+new_song.artist_name = artist_name
+
+new_song.name = song_name
+
+new_song.save
+
+new_song
+  
     
 end
 
